749b3ccaaa26d0d08c8e5477001b2e204b7c1f8c
BrandynM <brandyn.roatch@hotmail.com>
2022-03-20 01:09:41 -0700

    tried setting up page-based navigation after connecting watch, rethinking how to display player data.

---
+++ MummaBrandyn_WearableProject/Wearableroject_Watch Extension/InterfaceController_Player1.swift
@@ -0,0 +1,30 @@
+//
+//  InterfaceController_Player1.swift
+//  Wearableroject_Watch Extension
+//
+//  Created by Brandyn Roatch on 3/20/22.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController_Player1: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
